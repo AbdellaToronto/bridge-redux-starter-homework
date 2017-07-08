05de63c77d8c69d16bf036285076e1244e0fe1d7
jprittie <eyewallop@gmail.com>
2017-07-07 21:26:47 -0400

    Adds removeProduct

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,25 @@
 export const ACTION_TYPES = {
   addProduct: 'ADD_PRODUCTS',
+  removeProduct: 'REMOVE_PRODUCT',
 };
 
+
 export function addProduct(product) {
-  debugger;
+  //debugger;
   return {
     type: ACTION_TYPES.addProduct,
     payload: {
       product,
+    }
+  }
+}
+
+export function removeProduct(productID) {
+  //debugger;
+  return {
+    type: ACTION_TYPES.removeProduct,
+    payload: {
+      productID,
     }
   }
 }
