1bff1fb8c1faf7b3480b69279f1e2ce0d6df4719
jprittie <eyewallop@gmail.com>
2017-07-09 15:20:04 -0400

    Adds visibleProducts

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,23 +1,53 @@
+import { generateProducts } from '../utils/data';
 import { combineReducers } from 'redux';
 
-import { generateProducts } from '../utils/data';
 import { ACTION_TYPES } from '../actions';
+// bring in search reducer
+import { textInput } from './search';
 
 const INITIAL_STATE = {
   products: generateProducts(10),
 };
 
+
 export const products = (state = INITIAL_STATE.products, { type, payload }) => {
-  debugger;
   switch (type) {
     case ACTION_TYPES.addProduct:
-      debugger;
       return [...state, payload.product];
+    case ACTION_TYPES.removeProduct:
+      return state.filter((product) => product.id !== payload.productID);
+    default:
+      return state;
+  }
+};
+
+// how do I access the current state.products? through getState?
+export const visibleProducts = (state = INITIAL_STATE.products, {type, payload}) => {
+  //const { products } = getState();
+  switch(type) {
+    case ACTION_TYPES.inputChange:
+      console.log("payload.value is " + payload.value);
+
+      //check if payload has length... i.e. if user has typed a character
+      if ( (payload.value) && (payload.value.length) ) {
+        //then take a copy
+        const productsCopy = [...products];
+        console.log("productsCopy is " + productsCopy);
+
+        //then filter products in that copy
+        return productsCopy.filter(product => {
+          (product.name).includes(payload.value)
+        });
+      }
+    default:
+      //do I want to return state or copy of state.products?
+      return state;
   }
 
-  return state;
-};
+}
 
 export default combineReducers({
   products,
+  visibleProducts,
+  textInput
 });
