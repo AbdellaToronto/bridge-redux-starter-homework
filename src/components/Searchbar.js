862cf74951319226d7d4c16b83335ee118591eac
jprittie <eyewallop@gmail.com>
2017-07-09 15:18:33 -0400

    Fixes import of actions/search

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,41 +1,28 @@
 import React from 'react';
-import { searchProducts } from '../actions/search.js';
+import { inputChange } from '../actions/search.js';
 import { connect } from 'react-redux';
 
-//do I do the connect in this component or in App.js?
+
 const mapStateToProps = state => ({
-  products: state.products,
   textInput: state.textInput,
 });
 
-//shouldn't this be a function
+//shouldn't this be a function?
 const mapDispatchToProps = {
-  onTextInputChange: searchProducts,
+  onTextInputChange: inputChange,
 };
 
 
 const Searchbar = ({textInput, onTextInputChange}) => (
-
-
-      <div>
-        <h3>Search by product name</h3>
-
-          <input
-            value={textInput}
-            type="string"
-            className="input"
-            onChange={ev => onTextInputChange(ev.target.value)}
-          />
-
-      </div>
-
+  <div>
+    <h3>Search by product name</h3>
+    <input
+      value={textInput}
+      type="string"
+      className="input"
+      onChange={ev => onTextInputChange(ev.target.value)}
+    />
+  </div>
 )
-
-
-
-
-
-
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(Searchbar);
