6713985895970483a3189e5fe2cf5b5b4a8ccf4c
jprittie <eyewallop@gmail.com>
2017-07-17 23:04:50 -0400

    Updates action type names

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,53 +1,32 @@
 import { generateProducts } from '../utils/data';
 import { combineReducers } from 'redux';
+import { textInput } from './search';
+import { reducer as formReducer } from 'redux-form';
 
 import { ACTION_TYPES } from '../actions';
-// bring in search reducer
-import { textInput } from './search';
+
+
 
 const INITIAL_STATE = {
   products: generateProducts(10),
 };
 
 
-export const products = (state = INITIAL_STATE.products, { type, payload }) => {
-  switch (type) {
-    case ACTION_TYPES.addProduct:
-      return [...state, payload.product];
-    case ACTION_TYPES.removeProduct:
-      return state.filter((product) => product.id !== payload.productID);
+export const products = (state = INITIAL_STATE.products, action) => {
+  switch (action.type) {
+    case ACTION_TYPES.ADD_NEW_PRODUCT:
+      return [...state, action.payload.values];
+    case ACTION_TYPES.REMOVE_PRODUCT:
+      return state.filter((product) => product.id !== action.payload.productID);
     default:
       return state;
   }
 };
 
-// how do I access the current state.products? through getState?
-export const visibleProducts = (state = INITIAL_STATE.products, {type, payload}) => {
-  //const { products } = getState();
-  switch(type) {
-    case ACTION_TYPES.inputChange:
-      console.log("payload.value is " + payload.value);
 
-      //check if payload has length... i.e. if user has typed a character
-      if ( (payload.value) && (payload.value.length) ) {
-        //then take a copy
-        const productsCopy = [...products];
-        console.log("productsCopy is " + productsCopy);
-
-        //then filter products in that copy
-        return productsCopy.filter(product => {
-          (product.name).includes(payload.value)
-        });
-      }
-    default:
-      //do I want to return state or copy of state.products?
-      return state;
-  }
-
-}
 
 export default combineReducers({
   products,
-  visibleProducts,
-  textInput
+  textInput,
+  form: formReducer
 });
